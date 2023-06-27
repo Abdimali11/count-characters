013234f560c97c9ae65e4b103ec039b383fa8815
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-27 11:08:31 -0700

    this is the answer of count-characters

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Good Luck 💪🏾
+function count(char, str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i) === char) {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  console.log(count("a", "fikrcamp")); 
+  console.log(count("c", "bootcamp")); 
+  console.log(count("o", "fikrcamp coding bootcamp")); 
